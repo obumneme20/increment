0e7f6e10b318429a8158933141b46ea2dcded591
obumneme20 <obumdarlington20@gmail.com>
2020-03-20 05:26:28 +0100

    initial commit

---
+++ decrement.js
@@ -0,0 +1,7 @@
+var decrement = '';
+
+for (var i = 0; i < 5; i++) {
+    decrement = decrement + '#';
+    
+    console.log(decrement);
+}
---
+++ increment.js
@@ -0,0 +1,7 @@
+var increment = '';
+
+for (var i = 0; i < 6; i++) {
+    increment = increment + '#';
+    
+    console.log(increment);
+}
